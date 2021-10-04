bb89883819435687ce4d7b20ca43e916c6c306fc
Peiwand Ismaiel <P.Ismaiel@student.han.nl>
2021-10-04 18:03:39 +0200

    replaced test tool with tool from tutorial

---
+++ DialogueCringe/Assets/Tools/DialogueGraphView.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+public class DialogueGraphView : GraphView
+{
+    private readonly Vector2 defaultNodeSize = new Vector2(150, 200);
+    public DialogueGraphView()
+    {
+        styleSheets.Add(Resources.Load<StyleSheet>("DialogueGraph"));
+        SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+        
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+
+        var grid = new GridBackground();
+        Insert(0, grid);
+        grid.StretchToParentSize();
+
+        AddElement(GenerateEntryPointNode());
+    }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        var compatiblePorts = new List<Port>();
+
+        ports.ForEach((port) => 
+        {
+            if (startPort != port && startPort.node != port.node)
+            {
+                compatiblePorts.Add(port);
+            }
+        });
+        return compatiblePorts;
+    }
+
+    private Port GeneratePort(DialogueNode node, Direction portDirection, Port.Capacity capacity = Port.Capacity.Single)
+    {
+        return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float));
+    }
+
+    private DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "START",
+            GUID = Guid.NewGuid().ToString(),
+            dialogueText = "ENTRYPOINT",
+            entrypoint = true
+        };
+
+        var generatedNode = GeneratePort(node, Direction.Output);
+        generatedNode.portName = "Next";
+        node.outputContainer.Add(generatedNode);
+
+        node.RefreshExpandedState();
+        node.RefreshPorts();
+
+        node.SetPosition(new Rect(100, 200, 100, 150));
+        return node;
+    }
+
+    public void CreateNode(string nodeName)
+    {
+        AddElement(CreateDialogueNode(nodeName));
+    }
+
+    public DialogueNode CreateDialogueNode(string nodeName)
+    {
+        var dialogueNode = new DialogueNode
+        {
+            title = "Dialogue Node",
+            dialogueText = nodeName,
+            GUID = Guid.NewGuid().ToString(),
+        };
+        var inputPort = GeneratePort(dialogueNode, Direction.Input, Port.Capacity.Multi);
+        inputPort.portName = "Input";
+        dialogueNode.inputContainer.Add(inputPort);
+
+        var button = new Button(()=> { AddChoicePort(dialogueNode); });
+        button.text = "New Choice";
+        dialogueNode.titleContainer.Add(button);
+
+        dialogueNode.RefreshExpandedState();
+        dialogueNode.RefreshPorts();
+        dialogueNode.SetPosition(new Rect(Vector2.zero, defaultNodeSize));
+
+        return dialogueNode;
+    }
+
+    private void AddChoicePort(DialogueNode dialogueNode)
+    {
+        var generatedPort = GeneratePort(dialogueNode, Direction.Output);
+
+        var outputPortCount = dialogueNode.outputContainer.Query("connector").ToList().Count;
+        generatedPort.portName = $"Coice {outputPortCount}";
+
+        dialogueNode.outputContainer.Add(generatedPort);
+        dialogueNode.RefreshExpandedState();
+        dialogueNode.RefreshPorts();
+
+    }
+}
