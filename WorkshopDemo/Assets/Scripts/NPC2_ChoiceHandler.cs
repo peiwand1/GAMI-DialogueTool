4d69ac713ff39a54f0262481cec5af5db19be708
canibani <can.ugurlu@live.nl>
2021-10-26 16:08:32 +0200

    Some code cleaning and added the option to open left and right door for NPC2

--- WorkshopDemo/Assets/Scripts/NPC2_ChoiceHandler.cs
+++ WorkshopDemo/Assets/Scripts/NPC2_ChoiceHandler.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class NPC2_ChoiceHandler : ChoiceHandler
 {
-    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject middleDoor;
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
+
     
     // Handle the choices made in the conversation
     public override void HandleChoice(NodeLinkData choice) {
-        // Open Door
+        // Open Middle Door
         if (choice.TargetNodeGuid == "33d511c6-403c-4a47-ade0-d78aec74e5ab") {
-            DoorState state = door.GetComponent<DoorState>();
+            DoorState state = middleDoor.GetComponent<DoorState>();
+            state.Open();
+        }
+
+        // Open Left Door
+        if (choice.TargetNodeGuid == "") {
+            DoorState state = leftDoor.GetComponent<DoorState>();
+            state.Open();     
+        }
+
+        // Open Right Door
+        if (choice.TargetNodeGuid == "") {
+            DoorState state = rightDoor.GetComponent<DoorState>();
             state.Open();
         }
     }
